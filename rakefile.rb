6654df0c0b4d8cbcdb460162d5c5b4052e00c186
Marien Tom <tommarien@gmail.com>
2012-05-12 13:22:15 +0200

    Clean up of rakefile introduced clobber begin of environment setup

--- rakefile.rb
+++ rakefile.rb
@@ -1,11 +1,15 @@
+require 'rake/clean'
 require 'albacore'
 require 'version_bumper'
 
-@env_sourcepath = "./src/"
+@env_sourcepath = File.expand_path('./src')
+@env_sharedassemblyinfo = File.join("#{@env_sourcepath}", "SharedAssemblyInfo.cs")
 @env_projectname = "Nebula"
 @env_buildconfigname = "Release"
 
 @connectionstring = "Server=.\NOCTO;Integrated Security=true;Initial Catalog=Nebula"
+
+CLEAN.include("#{@env_sharedassemblyinfo}")
 
 def env_buildversion
   bumper_version.to_s
@@ -19,32 +23,39 @@ def env_buildfolderpath
   "Builds/#{env_projectfullname}/"
 end
 
+task :default => "env:setup"
+
+namespace :env do
+	desc "Setup your environment"
+	task :setup, [:name]  => "version:assemblyinfo" do |t, args|
+	
+	end
+end
+
 namespace :version do
-	desc "writes shared assembly info."
+	desc "writes shared assembly info"
 	assemblyinfo :assemblyinfo do |asm|
-	  asm.output_file = "#{@env_sourcepath}SharedAssemblyInfo.cs"
+	  asm.output_file = "#{@env_sharedassemblyinfo}"
 	  asm.version = env_buildversion
 	  asm.file_version = env_buildversion  
 	end
 end
 
 namespace :build do
-	desc "build the debug version of the solution."
+	desc "build the debug version of the solution"
 	msbuild :debug => "version:assemblyinfo" do |msb|
 		msb.properties = {:configuration => :Debug}
 		msb.targets = [:Clean, :Build]
 		msb.solution = "Nebula.sln"
 	end
   
-	desc "build the release version of the solution."
+	desc "build the release version of the solution"
 	msbuild :release => "version:assemblyinfo" do |msb|
 		msb.properties = {:configuration => :Release}
 		msb.targets = [:Clean, :Build]
 		msb.solution = "Nebula.sln"
 	end
 end
-
-task :default => "build:debug"
 
 namespace :db do
 	desc "runs all necessary migrations"
--- src/SharedAssemblyInfo.cs
+++
@@ -1,5 +0,0 @@
-using System.Reflection;
-using System.Runtime.InteropServices;
-[assembly: AssemblyVersion("0.0.0.1")]
-[assembly: AssemblyFileVersion("0.0.0.1")]
-
