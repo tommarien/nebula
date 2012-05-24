96172cd3ff4ec5a4fb9057c6e2261c607876d30b
Marien Tom <tommarien@gmail.com>
2012-05-24 22:43:01 +0200

    Initial Razor application with a simple style

---
+++ src/Nebula.MvcApplication/Helpers/MenuItemHelper.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text;
+using System.Web.Mvc;
+using System.Web.Mvc.Html;
+
+namespace Nebula.MvcApplication.Helpers
+{
+    public static class MenuItemHelper
+    {
+        public static MvcHtmlString MenuItem(this HtmlHelper helper, string linkText, string actionName, string controllerName)
+        {
+            string currentControllerName = (string) helper.ViewContext.RouteData.Values["controller"];
+            string currentActionName = (string) helper.ViewContext.RouteData.Values["action"];
+
+            var sb = new StringBuilder();
+
+            if (currentControllerName.Equals(controllerName, StringComparison.CurrentCultureIgnoreCase) &&
+                currentActionName.Equals(actionName, StringComparison.CurrentCultureIgnoreCase))
+                sb.Append("<li class=\"selected\">");
+            else
+                sb.Append("<li>");
+
+            sb.Append(helper.ActionLink(linkText, actionName, controllerName));
+            sb.Append("</li>");
+            return new MvcHtmlString(sb.ToString());
+        }
+    }
+}
---
+++ src/Nebula.MvcApplication/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Nebula.MvcApplication")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Nebula.MvcApplication")]
+[assembly: AssemblyCopyright("Copyright ©  2012")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("7f4ee4ea-8d61-40c7-b4be-e0f42b8353ff")]
