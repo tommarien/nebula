09b9e017b71b8d6b414953c9e388c335063024d6
Marien Tom <tommarien@gmail.com>
2012-06-14 08:26:23 +0200

    Add missing domain tests

---
+++ src/Nebula.UnitTests/Nebula.Domain/Registration/AccountTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using NUnit.Framework;
+using Nebula.Domain.Registration;
+using Nebula.Infrastructure;
+
+namespace Nebula.UnitTests.Nebula.Domain.Registration
+{
+    [TestFixture]
+    public class AccountTests
+    {
+        [Test]
+        public void ChangePassword_Should_not_allow_null_password()
+        {
+            var account = ObjectMother.CreateAccount("userx", "secret");
+
+            Assert.Throws<ArgumentNullException>(() => account.ChangePassword(null));
+        }
+
+        [Test]
+        [TestCase(null)]
+        [TestCase("")]
+        [TestCase(" ")]
+        public void Ctor_Should_not_allow_for_username(string username)
+        {
+            Assert.Throws<InvalidStateException>(() => new Account(username, new Password("secret")));
+        }
+
+        [Test]
+        public void Ctor_Should_not_allow_null_password()
+        {
+            Assert.Throws<ArgumentNullException>(() => new Account("johnDoe", null));
+        }
+    }
+}
