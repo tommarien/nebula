049bbb1cb3d64c70355a4b5c90dc76c6c810a52e
Marien Tom <tommarien@gmail.com>
2012-06-19 20:41:14 +0200

    ShowSql logging now configurable

--- config/defaults.rb
+++ config/defaults.rb
@@ -1,3 +1,6 @@
 # connectionstrings
 configatron.connectionstrings.nebula = 'Server=.\NOCTO;Integrated Security=true;Initial Catalog=Nebula;'
 configatron.connectionstrings.nebula_test = 'Server=.\NOCTO;Integrated Security=true;Initial Catalog=Nebula_Test;'
+
+# logging
+configatron.logging.showsql = false
