6f54bb1bfc2254f16ddff20932c6e358c9813e08
Marien Tom <tommarien@gmail.com>
2012-06-25 07:51:12 +0200

    Improved logging configuration

--- config/defaults.rb
+++ config/defaults.rb
@@ -3,4 +3,8 @@ configatron.connectionstrings.nebula = 'Server=.\NOCTO;Integrated Security=true;
 configatron.connectionstrings.nebula_test = 'Server=.\NOCTO;Integrated Security=true;Initial Catalog=Nebula_Test;'
 
 # logging
-configatron.logging.showsql = false
+configatron.logging.db.buffersize = 20
+configatron.logging.db.minimumlevel = 'WARN'
+configatron.logging.file.enabled = false
+configatron.logging.file.showsql = false
+configatron.logging.file.trace = false
