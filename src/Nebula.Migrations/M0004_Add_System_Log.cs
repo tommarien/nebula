9968e74be1bb5ae5fea74885d255977061cbd8ea
Marien Tom <tommarien@gmail.com>
2012-06-26 21:22:00 +0200

    Small fixes

--- src/Nebula.Migrations/M0004_Add_System_Log.cs
+++ src/Nebula.Migrations/M0004_Add_System_Log.cs
@@ -11,15 +11,17 @@ namespace Nebula.Migrations
         public override void Up()
         {
             Create.Table(TableName).InSchema(SchemaName)
+                // Default logging
                 .WithColumn("Id").AsInt32().Identity().PrimaryKey().NotNullable()
                 .WithColumn("Date").AsDateTime().NotNullable()
-                .WithColumn("HostName").AsAnsiString(255).NotNullable()
                 .WithColumn("Level").AsAnsiString(50).NotNullable()
                 .WithColumn("Logger").AsAnsiString(255).NotNullable()
                 .WithColumn("Message").AsAnsiString(2000).NotNullable()
                 .WithColumn("Exception").AsAnsiString(4000).Nullable()
-                .WithColumn("SessionId").AsAnsiString(255).NotNullable()
-                .WithColumn("UserName").AsAnsiString(255).NotNullable();
+                // Extended properties
+                .WithColumn("HostName").AsAnsiString(255).Nullable()
+                .WithColumn("SessionId").AsAnsiString(255).Nullable()
+                .WithColumn("UserName").AsAnsiString(255).Nullable();
         }
 
         public override void Down()
