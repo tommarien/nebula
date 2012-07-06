f6dcf3271bab25645f4b2c30dfdd59c0f4516cfc
Marien Tom <tommarien@gmail.com>
2012-07-06 07:59:14 +0200

    Cleanup of Logging.cshtml
    Changed Tabs settings for html and jscript
    Corrected Nebula.js

--- src/Nebula.MvcApplication/Scripts/Nebula.js
+++ src/Nebula.MvcApplication/Scripts/Nebula.js
@@ -1,36 +1,38 @@
-﻿// DataTable conventional setup
-jQuery.fn.dataTable.defaults.bFilter = false;
-jQuery.fn.dataTable.defaults.bLengthChange = false;
-jQuery.fn.dataTable.defaults.iDisplayLength = 10;
-jQuery.fn.dataTable.defaults.aoColumnDefs = [
-    {
-        'aTargets': ['_all'],
-        'bSearchable': false,
-        'bSortable': false
-    },
-    {
-        'aTargets': ['jq_dt_hidden'],
-        'bVisible': false
-    },
-    {
-        'aTargets': ['jq_dt_type_json_datetime'],
-        'fnRender': function (o, val) {
-            return parseAndFormatJsonDateTime(val);
-        }
-    },
-    {
-        'aTargets': ['jq_dt_type_pre_text'],
-        'fnRender': function (o, val) {
-            return replaceLineBreaks(val);
-        }
-    }
-];
+﻿(function ($) {
+	// DataTable conventional setup
+	$.fn.dataTable.defaults.bFilter = false;
+	$.fn.dataTable.defaults.bLengthChange = false;
+	$.fn.dataTable.defaults.iDisplayLength = 10;
+	$.fn.dataTable.defaults.aoColumnDefs = [
+		{
+			'aTargets': ['_all'],
+			'bSearchable': false,
+			'bSortable': false
+		},
+		{
+			'aTargets': ['jq_dt_hidden'],
+			'bVisible': false
+		},
+		{
+			'aTargets': ['jq_dt_type_json_datetime'],
+			'fnRender': function (o, val) {
+				return parseAndFormatJsonDateTime(val);
+			}
+		},
+		{
+			'aTargets': ['jq_dt_type_pre_text'],
+			'fnRender': function (o, val) {
+				return replaceLineBreaks(val);
+			}
+		}
+	];
+})(jQuery);
 
 // Functions
 function replaceLineBreaks(value) {
-    return (value) ? value.replace(/\n/g, '<br />') : '';
+	return (value) ? value.replace(/\n/g, '<br />') : '';
 }
 
 function parseAndFormatJsonDateTime(value) {
-    return (value) ? moment(value).format('DD/MM/YYYY HH:mm:ss') : '';
+	return (value) ? moment(value).format('DD/MM/YYYY HH:mm:ss') : '';
 }
