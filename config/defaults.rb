fcc0dd82ea106938a354a147a6e37d290b27eb33
Marien Tom <tommarien@gmail.com>
2012-07-20 07:55:18 +0200

    Removed connectionstrings from defaults.rb as they should reside in environment configs

--- config/defaults.rb
+++ config/defaults.rb
@@ -1,7 +1,3 @@
-# connectionstrings
-configatron.connectionstrings.nebula = 'Server=.\NOCTO;Integrated Security=true;Initial Catalog=Nebula;'
-configatron.connectionstrings.nebula_test = 'Server=.\NOCTO;Integrated Security=true;Initial Catalog=Nebula_Test;'
-
 # logging
 configatron.logging.db.buffersize = 20
 configatron.logging.db.minimumlevel = 'WARN'
