c4ccee9d473281e8f377a466a5a14d41670d6192
Tom Marien <tommarien@gmail.com>
2012-12-07 17:00:47 +0100

    Refactored out dependency to static FormsAuthentication, just to make it unittestable

--- src/Nebula.MvcApplication/LifecycleEventHandlers/RoleProvidingAuthenticateRequestEventHandler.cs
+++ src/Nebula.MvcApplication/LifecycleEventHandlers/RoleProvidingAuthenticateRequestEventHandler.cs
@@ -6,6 +6,7 @@ using System.Web.Security;
 using Nebula.Contracts.Registration;
 using Nebula.Infrastructure.Querying;
 using Nebula.MvcApplication.LifecycleEvents;
+using Nebula.MvcApplication.Services;
 
 namespace Nebula.MvcApplication.LifecycleEventHandlers
 {
@@ -13,10 +14,12 @@ namespace Nebula.MvcApplication.LifecycleEventHandlers
         IHttpApplicationLifecycleEventHandler<AuthenticateRequestEvent>
     {
         private readonly IQueryHandler<AccountQuery, Role[]> getRolesQuery;
+        private readonly IFormsAuthenticationService formsAuthenticationService;
 
-        public RoleProvidingAuthenticateRequestEventHandler(IQueryHandler<AccountQuery, Role[]> getRolesQuery)
+        public RoleProvidingAuthenticateRequestEventHandler(IQueryHandler<AccountQuery, Role[]> getRolesQuery, IFormsAuthenticationService formsAuthenticationService)
         {
             this.getRolesQuery = getRolesQuery;
+            this.formsAuthenticationService = formsAuthenticationService;
         }
 
         public void Handle(AuthenticateRequestEvent instance)
@@ -25,10 +28,10 @@ namespace Nebula.MvcApplication.LifecycleEventHandlers
 
             if (!httpContext.Request.IsAuthenticated) return;
 
-            HttpCookie authCookie = httpContext.Request.Cookies[FormsAuthentication.FormsCookieName];
+            HttpCookie authCookie = httpContext.Request.Cookies[formsAuthenticationService.FormsCookieName];
             if (authCookie == null) return;
 
-            FormsAuthenticationTicket authTicket = FormsAuthentication.Decrypt(authCookie.Value);
+            FormsAuthenticationTicket authTicket = formsAuthenticationService.Decrypt(authCookie.Value);
 
             Role[] roles = getRolesQuery.Execute(new AccountQuery {UserName = authTicket.Name});
             var user = new GenericPrincipal(instance.HttpContext.User.Identity,
--- src/Nebula.MvcApplication/Services/IFormsAuthenticationService.cs
+++ src/Nebula.MvcApplication/Services/IFormsAuthenticationService.cs
@@ -4,12 +4,26 @@ namespace Nebula.MvcApplication.Services
 {
     public interface IFormsAuthenticationService
     {
+        string FormsCookieName { get; }
+
+        FormsAuthenticationTicket Decrypt(string encryptedTicket);
+
         void SignIn(string userName, bool createPersistentCookie);
         void SignOut();
     }
 
     public class FormsAuthenticationService : IFormsAuthenticationService
     {
+        public string FormsCookieName
+        {
+            get { return FormsAuthentication.FormsCookieName; }
+        }
+
+        public FormsAuthenticationTicket Decrypt(string encryptedTicket)
+        {
+            return FormsAuthentication.Decrypt(encryptedTicket);
+        }
+
         public void SignIn(string userName, bool createPersistentCookie)
         {
             FormsAuthentication.SetAuthCookie(userName, createPersistentCookie);
